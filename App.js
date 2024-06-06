ffaa256696eee566cb8cb33a097c3013501183b5
Malik <malik.krijestorac@stu.ssst.edu.ba>
2024-06-06 19:39:38 +0200

    Modified navigation

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import PeopleList from './components/PeopleList';
-import PeopleDetails from './components/PeopleDetails';
+import PeopleList from './screens/PeopleList';
+import InvitationList from './screens/InvitationList';
 import Icon from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './screens/HomeScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,13 +16,13 @@ export default function App() {
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
-
             if (route.name === 'Home') {
               iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings' : 'settings-outline';
-            }
-
+            } else if (route.name === 'All Friends') {
+              iconName = focused ? 'people' : 'people-outline';
+            } else if (route.name === 'Invitations') {
+                iconName = focused ? 'mail' : 'mail-outline';
+            } 
             return <Icon name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: 'tomato',
@@ -31,16 +31,18 @@ export default function App() {
             backgroundColor: '#fff',
             borderTopWidth: 0,
             elevation: 5,
-            height: 60,
+            height: 72,
           },
           tabBarLabelStyle: {
-            fontSize: 12,
+            fontSize: 13,
             marginBottom: 5,
           },
+          headerShown: false,
         })}
       >
-        <Tab.Screen name="Home" component={PeopleList} />
-        <Tab.Screen name="Settings" component={PeopleDetails} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="All Friends" component={PeopleList} />
+        <Tab.Screen name="Invitations" component={InvitationList} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ screens/HomeScreen.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function HomeScreen() {
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+
+    const loadFonts = async () => {
+        await Font.loadAsync({
+            'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+        });
+        setFontsLoaded(true);
+        SplashScreen.hideAsync();
+    };
+
+    useEffect(() => {
+        loadFonts();
+    }, []);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titlePart1}>Welcome to our</Text>
+            <Text style={styles.titlePart2}>Party Invitation App</Text>
+            <StatusBar style='auto' />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    titlePart1: {
+        fontSize: 36,
+        fontFamily: 'Poppins-Bold',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    titlePart2: {
+        fontSize: 36,
+        fontFamily: 'Poppins-Bold',
+        textAlign: 'center',
+    },
+});
